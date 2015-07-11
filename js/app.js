4a856717bac211f5636d5c7d9af259254442ed82
Squeezeit <jaittariusrobinson@Jaittariuss-Mac-mini.local>
2015-07-11 16:57:15 -0400

    Added handleInput function
    Can now see the game board! Yay!

--- js/app.js
+++ js/app.js
@@ -36,6 +36,9 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Player.prototype.handleInput = function() {
+    
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
